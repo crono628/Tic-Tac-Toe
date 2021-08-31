17a07d91601fbcf8afdf09fd2b60e02eea35a3af
crono628 <84817352+crono628@users.noreply.github.com>
2021-08-31 19:57:49 +0000

    commit on main

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
-let turn
-const cells = document.querySelectorAll('[data-cell]')
+let xTurn = true;
+let oTurn = false;
+const cells = document.querySelectorAll('[data-cell]');
+const xClass = document.querySelector('.x');
+const oClass = document.querySelector('.o');
+const resetBtn = document.querySelector('reset-btn')
 
-const Player = (name, mark) => {
-    const winningCombo = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-    winnerCheck = (mark) => {
-        winningCombo.some(combo => {
-            combo.every(index => {
-                cells[index].classList.contains(mark)
-            })
-        })
-    }
-    return {
-        name,
-        mark,
-        winner
-    }
+const winningCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+const winnerCheck = (mark) => {
+  winningCombo.some(combo => {
+    combo.every(index => {
+      cells[index].classList.contains(mark)
+    })
+  })
 }
 
+const Player = (name, mark) => {
+  let dataPlayer = document.querySelectorAll('[data-player]')
+  let getName = (name) => dataPlayer.value
+  return {
+    name,
+    mark
+  }
+}
+
+cells.forEach((cell) => {
+  cell.addEventListener('click', (e) => {
+    if (cell.classList.contains('ready')) {
+      e.target.textContent = 'X'
+      e.target.classList.remove('hover', 'ready')
+    }
+  })
+  cell.addEventListener('mouseover', (e) => {
+    if (cell.classList.contains('ready')) {
+      e.target.textContent = 'X'
+      e.target.classList.add('hover')
+    }
+  })
+  cell.addEventListener('mouseleave', (e) => {
+    if (cell.classList.contains('ready')) {
+      e.target.textContent = ''
+      e.target.classList.remove('hover')
+    }
+  })
+})
+
 const playerOne = Player('mikey', 'x')
-playerOne.name
-console.log(playerOne.name);
+// console.log(playerOne.mark);
