59ca33f3b946991eedb62bacd9ed4b3e537ebf6f
Michael DeSantis <m.desantis628@gmail.com>
2021-08-30 22:46:14 -0400

    commit on main

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const GameBoardObject = () => {
-    gameBoard = [];
-    playerOne =
-        playerTwo =
+let turn
+const cells = document.querySelectorAll('[data-cell]')
+
+const Player = (name, mark) => {
+    const winningCombo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    winnerCheck = (mark) => {
+        winningCombo.some(combo => {
+            combo.every(index => {
+                cells[index].classList.contains(mark)
+            })
+        })
+    }
+    return {
+        name,
+        mark,
+        winner
+    }
 }
 
-function foo() {
-    return this
-}
+const playerOne = Player('mikey', 'x')
+playerOne.name
+console.log(playerOne.name);
