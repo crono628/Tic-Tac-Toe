8e064e6af6d9e3d9043a06b9ed767cb332123231
Michael DeSantis <m.desantis628@gmail.com>
2021-08-31 21:39:10 -0400

    commit on main

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
 let xTurn = true;
-let oTurn = false;
 const cells = document.querySelectorAll('[data-cell]');
-const xClass = document.querySelector('.x');
-const oClass = document.querySelector('.o');
 const resetBtn = document.querySelector('reset-btn')
+const playerOneBtn = document.querySelector('#player-one-btn')
+const playerTwoBtn = document.querySelector('#player-two-btn')
 
 const winningCombo = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-const winnerCheck = (mark) => {
-  winningCombo.some(combo => {
-    combo.every(index => {
-      cells[index].classList.contains(mark)
-    })
-  })
-}
-
 const Player = (name, mark) => {
-  let dataPlayer = document.querySelectorAll('[data-player]')
-  let getName = (name) => dataPlayer.value
-  return {
-    name,
-    mark
-  }
+    const dataPlayer = document.querySelectorAll('[data-player]')
+    dataPlayer.value = name
+    return {
+        name,
+        mark
+    }
 }
 
 cells.forEach((cell) => {
-  cell.addEventListener('click', (e) => {
-    if (cell.classList.contains('ready')) {
-      e.target.textContent = 'X'
-      e.target.classList.remove('hover', 'ready')
-    }
-  })
-  cell.addEventListener('mouseover', (e) => {
-    if (cell.classList.contains('ready')) {
-      e.target.textContent = 'X'
-      e.target.classList.add('hover')
-    }
-  })
-  cell.addEventListener('mouseleave', (e) => {
-    if (cell.classList.contains('ready')) {
-      e.target.textContent = ''
-      e.target.classList.remove('hover')
-    }
-  })
+    cell.addEventListener('click', (e) => {
+        if (cell.classList.contains('ready')) {
+            e.target.textContent = 'X'
+            e.target.classList.remove('hover', 'ready')
+        }
+    })
+    cell.addEventListener('mouseover', (e) => {
+        if (cell.classList.contains('ready')) {
+            e.target.textContent = 'X'
+            e.target.classList.add('hover')
+        }
+    })
+    cell.addEventListener('mouseleave', (e) => {
+        if (cell.classList.contains('ready')) {
+            e.target.textContent = ''
+            e.target.classList.remove('hover')
+        }
+    })
 })
 
 const playerOne = Player('mikey', 'x')
-// console.log(playerOne.mark);
+console.log(playerOne.mark);
