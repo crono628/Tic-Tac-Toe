340f3b767685661bc56bb630480fef304135717b
Michael DeSantis <m.desantis628@gmail.com>
2021-11-11 22:37:54 -0500

    working on getting winner

--- script.js
+++ script.js
@@ -1,91 +1,28 @@
-<<<<<<< HEAD
-=======
-const cells = document.querySelectorAll(".cell");
-const resetBtn = document.querySelector('.reset-btn')
+const boardArray = [];
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 let xTurn = true;
-let gameOn = true;
-
-const turn = () => {
-  if (xTurn == true) {
-    return "X";
-  } else if (xTurn == false) {
-    return "O";
-  }
-};
-
-const gameBoard = (() => {
-  let gameArray = ["", "", "", "", "", "", "", "", ""];
-  const winningCombo = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  const getArray = () => gameArray;
-  const ammendArray = (position, player) => {
-    cells.forEach((cell) => {
-      if (cell.classList.contains("ready")) {
-        gameArray.splice(position, 1, player);
-      }
-    });
-  };
-
-  let winnerCheck = (player) => {
-    return winningCombo.some(combination => {
-      return combination.every(index => {
-        return gameArray[index].includes(player);
-      });
-    });
-  };
-
-  let reset = () => {
-    gameArray = ["", "", "", "", "", "", "", "", ""]
-  }
-
-  let gameReset = () => {
-    reset()
-  }
-
-  return {
-    getArray,
-    ammendArray,
-    winnerCheck,
-    gameReset
-  };
-})();
-
+const cells = document.querySelectorAll('[data-square]');
 cells.forEach((cell) => {
-  cell.addEventListener("click", () => {
-    if (gameOn === true) {
-      if (cell.classList.contains("ready")) {
-        let index = cell.dataset.record;
-        cell.innerText = `${turn()}`;
-        cell.classList.remove("ready");
-        gameBoard.ammendArray(index, turn());
+  cell.addEventListener('click', (e) => {
+    if (cell.classList.contains('ready')) {
+      if (xTurn) {
+        cell.innerText = 'X';
+      } else {
+        cell.innerText = 'O';
       }
+      xTurn = !xTurn;
+      e.target.classList.remove('ready');
+      console.log(xTurn);
     }
-    if (gameBoard.winnerCheck(turn())) {
-      cells.forEach((cell) => {
-        cell.classList.add('spin')
-      });
-      gameOn = false;
-    }
-    xTurn = !xTurn;
   });
 });
-
-resetBtn.onclick = function () {
-  gameOn = true
-  xTurn = true
-  gameBoard.gameReset()
-  cells.forEach(cell => {
-    cell.innerText = ''
-    cell.classList.add('ready')
-    cell.classList.remove('spin')
-  })
-}
->>>>>>> f8a3cfaabceb0cdd114cbe354ce07059aa164f8c
