676d698b520f3ef9d49b546244ad32c9e820f719
Michael DeSantis <m.desantis628@gmail.com>
2021-11-12 21:12:16 -0500

    logic complete. missing shine

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const boardArray = [];
 const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,6 +11,7 @@ const winningCombos = [
 
 let xTurn = true;
 const cells = document.querySelectorAll('[data-square]');
+const button = document.querySelector('button');
 cells.forEach((cell) => {
   cell.addEventListener('click', (e) => {
     if (cell.classList.contains('ready')) {
@@ -22,7 +22,41 @@ cells.forEach((cell) => {
       }
       xTurn = !xTurn;
       e.target.classList.remove('ready');
-      console.log(xTurn);
+      e.target.classList.add(`${cell.innerText}`);
+      if (checkDraw()) {
+        button.style.display = 'block';
+      } else {
+        if (checkWin(`${cell.innerText}`)) {
+          console.log('winner');
+          cells.forEach((cell) => {
+            cell.classList.remove('ready');
+            button.style.display = 'block';
+          });
+        }
+      }
     }
   });
 });
+
+function checkWin(player) {
+  return winningCombos.some((combo) => {
+    return combo.every((index) => {
+      return cells[index].classList.contains(player);
+    });
+  });
+}
+
+function checkDraw() {
+  return Array.from(cells).every((cell) => !cell.classList.contains('ready'));
+}
+
+function newGame() {
+  cells.forEach((cell) => {
+    cell.className = 'cell ready';
+    cell.innerText = '';
+    xTurn = true;
+    button.style.display = 'none';
+  });
+}
+
+button.onclick = newGame;
